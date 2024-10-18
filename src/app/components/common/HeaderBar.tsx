e975f1d3b9f92eda96b61d4b4aa4844b91f383a9
pdemeu <pdemeu.pessoal@gmail.com>
2024-10-17 23:01:18 -0300

    Feat: Create Header

---
+++ src/app/components/common/HeaderBar.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import Logo from "@/images/logo.png";
+import Search from "@/icons/search.svg"
+
+function HeaderBar() {
+  return (
+    <nav className="bg-white w-full h-[80px] flex items-center">
+      <div className="w-9/12 flex items-center justify-between mx-auto">
+        <Image src={Logo} alt="Capputeeno Logo" width={220} height={80} />
+
+        <div className="flex items-center justify-between">
+          <div className="flex items-center justify-between bg-[#F3F5F6] rounded px-4 py-3 gap-6">
+            <input
+              id="search"
+              name="search"
+              placeholder="Procurando por algo específico?"
+              className="border-none bg-transparent text-gray-600 w-80"
+            />
+            <button>
+              <Image src={Search} alt="Icone de buscar" width={24} height={24} />
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default HeaderBar;
