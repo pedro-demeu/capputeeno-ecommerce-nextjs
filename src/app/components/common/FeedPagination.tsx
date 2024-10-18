e89ff225ec78dde194c7c973bc4a20724b12a4c8
pdemeu <pdemeu.pessoal@gmail.com>
2024-10-17 23:39:32 -0300

    Feat: create feed pagination component v1

---
+++ src/app/components/common/FeedPagination.tsx
@@ -0,0 +1,45 @@
+"use client";
+import { useState, useMemo } from "react";
+
+function FeedPagination() {
+  const [activePage, setActivePage] = useState(0);
+
+  const pages = useMemo(() => {
+    return Array.from({ length: 5 }).map((_, index) => ({
+      id: index + 1,
+      value: index,
+    }));
+  }, []);
+
+  return (
+    <div className="flex items-center gap-1">
+      {pages?.map((page) => (
+        <button
+          key={`${page.id}-page`}
+          className={`w-[32px] h-[32px] rounded-md font-light transition-colors 
+            ${
+              activePage === page.value
+                ? "bg-gray-100 border border-[#FFA585] text-[#FFA585]"
+                : "bg-gray-100 border border-transparent text-gray-600"
+            }`}
+          onClick={() => setActivePage(page.value)}
+        >
+          {page.value + 1}
+        </button>
+      ))}
+      <button
+          className="w-[32px] h-[32px] rounded-md font-light transition-colors                 bg-gray-100 border border-transparent text-gray-600"
+        >
+          {"<"}
+        </button>
+        <button
+                   className="w-[32px] h-[32px] rounded-md font-light transition-colors                 bg-gray-100 border border-transparent text-gray-600"
+
+        >
+          {">"}
+        </button>
+    </div>
+  );
+}
+
+export default FeedPagination;
