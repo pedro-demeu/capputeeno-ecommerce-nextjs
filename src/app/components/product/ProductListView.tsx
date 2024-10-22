c3d095f79740188524ed78444b380a50fa22200a
pdemeu <pdemeu.pessoal@gmail.com>
2024-10-22 00:13:56 -0300

    Feat: Add checks for http requests

--- src/app/components/product/ProductListView.tsx
+++ src/app/components/product/ProductListView.tsx
@@ -2,17 +2,20 @@ import { Product } from "@/app/types/product";
 import ProductList from "./ProductList";
 
 async function getProducts() {
-  const res = await fetch("http://localhost:3001/products");
-  const data = await res.json();
-  return data;
+  try {
+    const res = await fetch("http://localhost:3001/products");
+    const data = await res.json();
+    return data;
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 async function ProductListView() {
   const products: Product[] = await getProducts();
 
+  if (!products) return <h1>Dados indisponíveis</h1>;
   return <ProductList products={products} />;
 }
-
-
 
 export default ProductListView;
